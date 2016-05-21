43fb53e40097ff7e7e863c8d6e4e4d3f845e95f1
Robert Strandh <robert.strandh@gmail.com>
2016-05-21 08:19:13 +0200

    Define parser combinator class SATISFIES.

--- Combinators/combinators.lisp
+++ Combinators/combinators.lisp
@@ -16,3 +16,23 @@
 (defmethod parse ((parser bind) input)
   (loop for (result . rest) in (parse (parser parser) input)
 	append (parse (funcall (function parser) result) rest)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Parser class SATISFIES.
+
+(defclass satisfies (parser)
+  ((%predicate :initarg :predicate :reader predicate)
+   (%arguments :initarg :arguments :reader arguments)))
+
+(defun satisfies (predicate &rest arguments)
+  (make-instance 'satisfies
+    :predicate predicate
+    :arguments arguments))
+
+(defmethod parse ((parser satisfies) input)
+  (bind (item) 
+	(lambda (x) 
+	  (if (apply (predicate parser) x (arguments parser))
+	      (identity x)
+	      (fail)))))
