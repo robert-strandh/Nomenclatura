e598344f2b954e2a2b9fd954991ad5daef2021d8
Robert Strandh <robert.strandh@gmail.com>
2016-05-21 08:17:21 +0200

    Define primitive parser class IDENTITY.

--- Combinators/primitive-parsers.lisp
+++ Combinators/primitive-parsers.lisp
@@ -1,1 +1,14 @@
 (cl:in-package #:nomenclatura-combinators)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Parser class IDENTITY.
+
+(defclass identity (parser)
+  ((%value :initarg :value :reader value)))
+
+(defun identity (value)
+  (make-instance 'identity :value value))
+
+(defmethod parse ((parser identity) input)
+  (list (cons (value parser) input)))
