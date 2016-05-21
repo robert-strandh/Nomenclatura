7d061a7cceacf0e5bff96c4b299efb9e29d550e3
Robert Strandh <robert.strandh@gmail.com>
2016-05-21 08:19:01 +0200

    Define parser combinator class BIND.

--- Combinators/combinators.lisp
+++ Combinators/combinators.lisp
@@ -1,1 +1,18 @@
 (cl:in-package #:nomenclatura-combinators)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Parser class BIND.
+
+(defclass bind (parser)
+  ((%parser :initarg :parser :reader parser)
+   (%function :initarg :function :reader function)))
+
+(defun bind (parser function)
+  (make-instance 'bind
+    :parser parser
+    :function function))
+
+(defmethod parse ((parser bind) input)
+  (loop for (result . rest) in (parse (parser parser) input)
+	append (parse (funcall (function parser) result) rest)))
