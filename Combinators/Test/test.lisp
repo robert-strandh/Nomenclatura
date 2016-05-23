c396bbc1f26ebfd435994d6eb98c6bf7a20888e2
Robert Strandh <robert.strandh@gmail.com>
2016-05-23 09:32:58 +0200

    Define methods on generic functions for input to work on lists.

--- Combinators/Test/test.lisp
+++ Combinators/Test/test.lisp
@@ -1,1 +1,10 @@
 (cl:in-package #:nomenclatura-combinators-test)
+
+(defmethod nclc:input-empty-p (input)
+  (endp input))
+
+(defmethod nclc:input-first (input)
+  (first input))
+
+(defmethod nclc:input-rest (input)
+  (rest input))
